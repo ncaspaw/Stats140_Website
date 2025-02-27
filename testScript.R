8a3318185cc3aaee585994031da64bd05a2932ae
ncaspaw <ncp1013@gmail.com>
2025-02-26 23:25:38 -0800

    Titles

--- testScript.R
+++ testScript.R
@@ -1,10 +1,12 @@
 x <- rnorm(100, mean = 5, sd = 2)
 median(x)
 # Terminal
+# 
+# Pushing code
 # git add .
 # git commit -m "Message"
-# git branch -M main
 # git push -u origin main
 
 # gitcreds::gitcreds_set()
+# git branch -M main
 
