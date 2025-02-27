52a1d4f58af49a9b69a1a477addb420b96799791
ncaspaw <ncp1013@gmail.com>
2025-02-26 23:24:49 -0800

    Calc median

--- testScript.R
+++ testScript.R
@@ -1,8 +1,10 @@
-rnorm(100, mean = 5, sd = 2)
-
+x <- rnorm(100, mean = 5, sd = 2)
+median(x)
 # Terminal
 # git add .
 # git commit -m "Message"
-# 
+# git branch -M main
+# git push -u origin main
 
-gitcreds::gitcreds_set()
+# gitcreds::gitcreds_set()
+
